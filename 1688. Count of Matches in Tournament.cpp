81ea24e8c2b63906af69ae8dd2d244d10c7b912a
Supan Roy <140494154+Supan-Roy@users.noreply.github.com>
2025-05-18 00:02:12 +0600

    Update 1688. Count of Matches in Tournament.cpp

--- 1688. Count of Matches in Tournament.cpp
+++ 1688. Count of Matches in Tournament.cpp
@@ -1,7 +1,26 @@
+//Solution 1 (Clever Way)
 class Solution {
 public:
     int numberOfMatches(int n) {
         return n-1;
         //What a FUN XD
+    }
+};
+
+//Solution 2 (Procedural Method)
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        int matches=0;
+        while (n>1) {
+            if (n%2==0) {
+                matches+=n/2;
+                n/=2;
+            } else{
+                matches+=(n-1)/2;
+                n=(n-1)/2+1;
+            }
+        }
+        return matches;
     }
 };
