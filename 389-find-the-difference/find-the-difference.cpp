2150b5f7fc4ae020b74cfe710f86d86a2834987c
Supan Roy <140494154+Supan-Roy@users.noreply.github.com>
2025-09-15 10:40:53 +0600

    Time: 3 ms (15.18%) | Memory: 9.3 MB (28.63%) - LeetSync

--- 389-find-the-difference/find-the-difference.cpp
+++ 389-find-the-difference/find-the-difference.cpp
@@ -1,13 +1,28 @@
+// class Solution {
+// public:
+//     char findTheDifference(string s, string t) {
+//         char res = 0;
+//         for(char c:s){
+//             res^=c;
+//         }
+//         for(char c:t){
+//             res^=c;
+//         }
+//         return res;
+//     }
+// };
+
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        char res = 0;
-        for(char c:s){
-            res^=c;
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+
+        for(int i=0;i<s.size();i++){
+            if(s[i]!=t[i]){
+                return t[i];
+            }
         }
-        for(char c:t){
-            res^=c;
-        }
-        return res;
+        return t.back();
     }
 };
