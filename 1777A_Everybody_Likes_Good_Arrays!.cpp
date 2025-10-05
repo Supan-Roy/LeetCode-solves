360cf253cfe1f91a4c8835895671445d4728c346
Supan Roy <140494154+Supan-Roy@users.noreply.github.com>
2025-10-05 21:40:31 +0600

    Initial commit

---
+++ 1777A_Everybody_Likes_Good_Arrays!.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int>a(n);
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        int count=0;
+        for(int i=0;i<n-1;i++){
+            if(a[i]%2==a[i+1]%2){
+                count++;
+            }
+        }
+        cout<<count<<endl;
+    }
+    return 0;
+}
