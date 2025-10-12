57fef671326338237548bac42eafad52cac90983
Supan Roy <140494154+Supan-Roy@users.noreply.github.com>
2025-10-12 22:28:45 +0600

    Time: 0 ms (100.00%) | Memory: 11.3 MB (80.08%) - LeetSync

---
+++ 1137-height-checker/height-checker.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector<int>ex(heights);
+        sort(heights.begin(), heights.end());
+        int count=0;
+        for(int i=0; i<heights.size(); i++){
+            if(heights[i]!=ex[i]){
+                count++;
+            }
+        }
+        return count;
+    }
+};
