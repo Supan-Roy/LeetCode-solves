3202afe4a27e919f9abc011590e1d8eee8e49a6f
Supan Roy <140494154+Supan-Roy@users.noreply.github.com>
2025-11-17 12:58:19 +0600

    Time: 0 ms (100.00%) | Memory: 32.5 MB (79.90%) - LeetSync

--- 4058-compute-alternating-sum/compute-alternating-sum.cpp
+++ 4058-compute-alternating-sum/compute-alternating-sum.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int alternatingSum(vector<int>& nums) {
-        int ans;
+        int ans=0;
         for(int i=0;i<nums.size();i++){
             if(i%2==0){
                 ans+=nums[i];
