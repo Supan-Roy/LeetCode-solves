68e3cccbb8a728cbd1a2159367b08ce7ffca7ab8
Supan Roy <140494154+Supan-Roy@users.noreply.github.com>
2026-02-04 20:27:01 +0600

    Time: 0 ms (100.00%) | Memory: 23.5 MB (33.44%) - LeetSync

---
+++ 155-min-stack/min-stack.cpp
@@ -0,0 +1,31 @@
+class MinStack {
+private:
+    stack<int> stk;
+    stack<int>minStk;
+public:
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        stk.push(val);
+        if(minStk.empty() || val<=minStk.top()){
+            minStk.push(val);
+        }
+    }
+    
+    void pop() {
+        if(stk.top()==minStk.top()){
+            minStk.pop();
+        }
+        stk.pop();
+    }
+    
+    int top() {
+        return stk.top();
+    }
+    
+    int getMin() {
+        return minStk.top();
+    }
+};
