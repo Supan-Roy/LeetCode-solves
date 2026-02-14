3f929745f2b93d064a1bc693acae1ef6fa07b508
Supan Roy <140494154+Supan-Roy@users.noreply.github.com>
2026-02-14 19:46:48 +0600

    Time: 0 ms (100.00%) | Memory: 17.1 MB (91.75%) - LeetSync

--- 2727-number-of-senior-citizens/number-of-senior-citizens.cpp
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.cpp
@@ -3,7 +3,7 @@ public:
     int countSeniors(vector<string>& details) {
         int count=0;
         for(auto &x:details){
-            if(x[11]>='6' && x[12]>'0' || x[11]>='7'){
+            if(x[11]>'6' || (x[11]=='6' && x[12]>'0')){
                 count++;
             }
         }
