c3c88ef27bdeb9051adeda79dfbd0d3417822870
Supan Roy <140494154+Supan-Roy@users.noreply.github.com>
2026-02-14 19:44:02 +0600

    Time: 0 ms (100.00%) | Memory: 17.3 MB (37.00%) - LeetSync

---
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int count=0;
+        for(auto &x:details){
+            if(x[11]>='6' && x[12]>'0' || x[11]>='7'){
+                count++;
+            }
+        }
+        return count;
+    }
+};
